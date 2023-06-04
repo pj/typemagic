12d37902eab282057197bd8944466caf134a93f9
Paul Johnson <paul@johnson.kiwi.nz>
2023-06-03 17:02:39 -0700

    WIP

--- flake.nix
+++ flake.nix
@@ -3,18 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-      deps = rec {
-        node = pkgs.nodejs_20
-        default = node;
-      };
-    in
-      {
-        packages.aarch64-darwin = deps;
-        devShells.aarch64-darwin.default = pkgs.mkShell { packages = pkgs.lib.attrsets.attrValues deps; };
-      };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = rec {
+                node = pkgs.nodejs_20;
+                default = node;
+            }; 
+        in 
+        {
+            packages = deps;
+            devShells = rec {
+              default = pkgs.mkShell { packages = pkgs.lib.attrsets.attrValues deps; };
+            };
+        }
+      );
 }
